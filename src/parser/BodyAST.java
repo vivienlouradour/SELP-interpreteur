940015d5f57bcd9272870c0bccca26c1f85a11e1
vivien <vivien@LRD-03-Debian.local>
2018-01-15 20:17:58 +0100

    debut

--- src/parser/BodyAST.java
+++ src/parser/BodyAST.java
@@ -1,8 +1,14 @@
 package parser;
 
+import lexer.Token;
+
 import java.util.ArrayList;
 
 public class BodyAST extends AST{
     private List<VarDef> vDefs = new ArrayList<>();
     private ExpressionAST exp;
+
+    public static BodyAST parse(Token token){
+        
+    }
 }
